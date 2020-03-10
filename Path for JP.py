4e8ba8824d4add20ccb4c6a726fdb0edf0c15ee6
Yantong Li <49718983+jerilyt@users.noreply.github.com>
2020-03-10 15:07:47 -0400

    install new packages in Jupyter Notebook

---
+++ Path for JP.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Path for Python code is different from path for Jupyter Notebook
+
+# In[1]:
+
+
+import sys
+
+
+# In[2]:
+
+
+sys.path
+
+
+# In jupter notebook, when we can not use the package which are downloaded, we should change the directory in Terminal. 
+# * Find the path by
+# > import sys
+# sys.path
+# 
+# * In Terminal
+# > cd //Users/rihiko/anaconda3/envs/tensorflow_env/bin
